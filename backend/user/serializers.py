08a60979ae4c5766a3891e50195403fc1561f9ef
DawitMebrahtu <dawitmebrahtu93@gmail.com>
2023-02-01 01:27:09 -0800

    Update user serializer

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 
 from user.models import User
 
-"""
 class UserSerializer(serializers.ModelSerializer):
     created = serializers.DateTimeField(read_only=True)
     modified = serializers.DateTimeField(read_only=True)
@@ -10,4 +9,3 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'created', 'modified']
-"""
