1d90354bc8c9c6296bc243d22a7ad3a47b5e3dae
Dawit Mebrahtu <91518911+DawitMebrahtu@users.noreply.github.com>
2023-02-13 01:49:10 -0800

    update validators

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -68,7 +68,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, "db.sqlite3")
     }
 }
-"""
+
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator", },
     {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator", },
@@ -85,7 +85,7 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 10
 }
-
+"""
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
