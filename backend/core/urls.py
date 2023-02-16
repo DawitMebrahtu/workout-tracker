4eb79b0bedd84391c61e5e94819082af44848b3e
Dawit Mebrahtu <91518911+DawitMebrahtu@users.noreply.github.com>
2023-02-16 03:09:28 -0800

    update excercise and workout

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,7 +5,6 @@ from core.views import ProfileViewSet, ExerciseViewSet, WorkoutViewSet
 core_router = SimpleRouter()
 
 core_router.register(r'profile', ProfileViewSet, basename='profile')
-"""
+
 core_router.register(r'exercise', ExerciseViewSet, basename='exercise')
 core_router.register(r'workout', WorkoutViewSet, basename='workout')
-"""
