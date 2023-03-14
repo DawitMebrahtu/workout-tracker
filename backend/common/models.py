b312d5cf7a34dcbd3453664797ba13971f1244b7
Dawit Mebrahtu <91518911+DawitMebrahtu@users.noreply.github.com>
2023-03-14 09:46:06 +0300

    Update models.py

--- backend/common/models.py
+++ backend/common/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from model_utils.fields import AutoCreatedField, AutoLastModifiedField
 
 
-
 class IndexedTimeStampedModel(models.Model):
     created = AutoCreatedField("created", db_index=True)
     modified = AutoLastModifiedField("modified", db_index=True)
